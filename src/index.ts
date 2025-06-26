84a5930e123327cf4d7dc66a321a498e20e42c02
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-06-26 17:30:09 +0530

    json requests and responsese

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { handlerMetrics } from "./handlers/no_of_requests.js";
 import { handlerReset } from "./handlers/reset.js";
 import { middlewareMetricsInc } from "./middlewares/fileServerHits.js";
 import { middlewareLogResponses } from "./middlewares/logResponses.js";
+import { handlerValidateChirps } from "./handlers/validate_chirp.js";
 
 const app = express();
+app.use(express.json());
 const PORT = 8080;
 
 // app.use(express.static("."));
@@ -16,8 +18,10 @@ app.get("/api/healthz", handlerReadiness);
 // app.get("/api/metrics", handlerMetrics);
 app.get("/admin/metrics", handlerMetrics);
 // app.get("/api/reset", handlerReset);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
+
+app.post("/api/validate_chirp", handlerValidateChirps);
 
 app.listen(PORT, () => {
-  console.log(`Server is running at https://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
