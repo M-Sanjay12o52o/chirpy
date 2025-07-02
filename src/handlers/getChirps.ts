e4d7fcac83307b1b8bb2d59d9b2530d50a8f4442
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-07-02 11:41:06 +0530

    optional query parameter 'sort'

--- src/handlers/getChirps.ts
+++ src/handlers/getChirps.ts
@@ -1,4 +1,4 @@
-import { asc, eq } from "drizzle-orm";
+import { asc, desc, eq } from "drizzle-orm";
 import { db } from "../db/index.js";
 import type { Request, Response } from "express";
 import { chirps, users } from "../db/schema.js";
@@ -13,19 +13,25 @@ The returned json doesn't contain all the chirps.
 
 export async function handlerGetChirps(req: Request, res: Response) {
   const authorId = req.query.authorId as string | undefined;
+  const sort = (req.query.sort as "asc" | "desc") || "asc";
 
   if (authorId) {
     const result = await db.query.chirps.findMany({
       // orderBy: (c) => [c.createdAt.asc],
       where: eq(chirps.userId, authorId),
-      orderBy: (user) => [asc(user.createdAt)],
+      orderBy: (user) => [
+        sort === "asc" ? asc(user.createdAt) : desc(user.createdAt),
+      ],
     });
 
     res.status(200).json(result);
   } else {
     const result = await db.query.chirps.findMany({
       // orderBy: (c) => [c.createdAt.asc],
-      orderBy: (user) => [asc(user.createdAt)],
+      // orderBy: (user) => [asc(user.createdAt)],
+      orderBy: (user) => [
+        sort === "asc" ? asc(user.createdAt) : desc(user.createdAt),
+      ],
     });
 
     res.status(200).json(result);
